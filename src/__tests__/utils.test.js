0b73ac7d7b25630550d2e4cfff96894352871f90
Bilha <bilha.leposo@student.moringaschool.com>
2024-05-04 18:24:17 +0300

    completes writing unit tests lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,42 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true for a palindrome word", () => {
+
+    expect(isPalindrome("racecar")).toBe(true);
+
+  });
+
+  it("returns false for a non-palindrome word", () => {
+
+    expect(isPalindrome("car")).toBe(false);
+
+  });
+
+  it("returns true for words with both uppercase and lowercase letters", () => {
+
+    expect(isPalindrome("RaCeCAr")).toBe(true);
+  });
+
+  it("returns false for an empty string", () => {
+
+    expect(isPalindrome("")).toBe(false);
+
+  });
+
+  it("throws an error for input that isn't a string", () => {
+
+    expect(() => {
+      isPalindrome(12345);
+    }).toThrow();
+
+  });
+
+  it("throws an error for input with non-alphabetic characters", () => {
+    expect(() => {
+      isPalindrome("12345");
+    }).toThrow();
+
+  });
+
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,24 @@
-// Your code here
+ export function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error("Input a string");
+
+    }
+    
+    if (word === "") {
+      return false; 
+
+    }
+    
+    if (!word.match(/^[A-Za-z]+$/)) {
+      throw new Error("Input alphabetic characters");
+
+    }
+    
+
+    const cleanedWord = word.toLowerCase().replace(/[^a-z]/g, '');  // convert word to lowercase and remove non-alphabetic characters
+    
+    const reversedCleanedWord = cleanedWord.split('').reverse().join('');   // Reverse the cleaned word
+
+    return cleanedWord === reversedCleanedWord;   // Check if the cleaned word is equal to its reversed word
+
+  }
